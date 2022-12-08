ca21611df49482b3d8d204c4e9a8459aacc91b33
임왕성 <76050934+ws3494@users.noreply.github.com>
2022-12-08 21:14:05 +0900

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Square.css";
+
+const Square = ({ onClick, value }) => {
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        onClick();
+      }}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
